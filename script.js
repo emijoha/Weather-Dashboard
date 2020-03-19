fd149cfdadf3a667abfc557a8d08740f882456f5
emi <ejhartline@gmail.com>
2020-03-19 12:30:37 -0400

    starting html wireframe, starting skeleton and custom css, starting ajax call in js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Query URL
+var apiKey = "2b3fd2d1da58a021b07a4c6c75acd193";
+var cityName = "Richmond"
+var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+
+console.log(queryURL);
