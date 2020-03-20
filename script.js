b8dce1c40716b74be6fc267a48dec69ef516902c
emi <ejhartline@gmail.com>
2020-03-20 18:13:50 -0400

    rest of current day's forecast renders, including UV index

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
 
+    // DEFINING VARIABLES AND FUNCTIONS
+    // __________________________________________________________________________________________________________________________
+
     // DOM elements
     var cityInputEl = $("#cityInput");
     var searchButton = $("button");
@@ -7,29 +10,42 @@ $(document).ready(function () {
     var cityNameDisplay = $("#cityDisplay");
     var dateDisplay = $("#dateDisplay");
     var weatherIcon = $("#weatherIcon");
+    var tempDisplay = $("#temp");
+    var humidDisplay = $("#humid");
+    var windDisplay = $("#wind");
+    var uvDisplay = $("#uv");
+    var uvIndex = $("#uvIndex");
+
+    // Moment.js
+    var today = moment().format("l");
+    var tomorrow = moment().add(1, "days").format("l");
+    var twoDays = moment().add(2, "days").format("l");
+    var threeDays = moment().add(3, "days").format("l");
+    var fourDays = moment().add(4, "days").format("l");
+    var fiveDays = moment().add(5, "days").format("l");
 
     // Event function
     var searchCity = function () {
+
         event.preventDefault();
+
+        // execture first ajax call
+        ajaxOne();
+
+    };
+
+    // function AJAX call 1
+    var ajaxOne = function () {
 
         // Query URL variables
         var apiKey = "2b3fd2d1da58a021b07a4c6c75acd193";
-        var cityName;
-        var queryURL;
+        var cityName = cityInputEl.val().trim();
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
 
-        // Moment.js
-        var today = moment().format("l");
-        var tomorrow = moment().add(1, "days").format("l");
-        var twoDays = moment().add(2, "days").format("l");
-        var threeDays = moment().add(3, "days").format("l");
-        var fourDays = moment().add(4, "days").format("l");
-        var fiveDays = moment().add(5, "days").format("l");
+        // store city: 
+        localStorage.setItem("city", cityName);
 
-        // redefine query variables to match city input
-        cityName = cityInputEl.val().trim();
-        queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
-
-        // AJAX call
+        // AJAX call 1
         $.ajax({
 
             url: queryURL,
@@ -48,20 +64,62 @@ $(document).ready(function () {
             var iconID = response.weather[0].icon;
             var iconURL = "http://openweathermap.org/img/wn/" + iconID + "@2x.png";
 
-            // populate DOM
+            // city's coordinates needed for UV index (ajax call 2)
+            localStorage.setItem("lat", response.coord.lat);
+            localStorage.setItem("lon", response.coord.lon);
+
+            // display in HTML
             cityNameDisplay.text(cityName + " ");
             dateDisplay.text(" (" + today + ") ");
             weatherIcon.attr("src", iconURL);
+            tempDisplay.text("Temperature: " + tempF.toFixed(1) + " \xB0F");
+            humidDisplay.text("Humidity: " + humidity + "%");
+            windDisplay.text("Wind Speed: " + windMPH + "mph");
 
             // create search history button
             var cityHistory = $("<p>").text(cityName).attr("class", "city-history");
             cityHistoryDiv.append(cityHistory);
+            // TO DO: conditionals for limiting number of renders and repeats
 
+            // execute second AJAX call
+            ajaxTwo();
 
         });
     };
 
-    // Event listener
+    // function AJAX call 2
+    var ajaxTwo = function () {
+
+        // query URL variables (2nd AJAX call)
+        var apiKey = "2b3fd2d1da58a021b07a4c6c75acd193";
+        var lat = localStorage.getItem("lat");
+        var lon = localStorage.getItem("lon");
+        var uvQueryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
+
+        $.ajax({
+
+            url: uvQueryURL,
+            method: "GET"
+
+        }).then(function (response2) {
+
+            console.log(response2);
+
+            // API variables
+            var uv = response2.value;
+
+            // display in HTML
+            uvDisplay.text("UV Index: ");
+            uvIndex.text(uv);
+            // TO DO: conditionals for color coding uv index display background
+
+        })
+    };
+
+    // EXECUTING FUNCTIONS
+    // __________________________________________________________________________________________________________________________
+
+    // Event listener executes searchCity, and its nested ajax call functions
     searchButton.on("click", searchCity);
 
 });
