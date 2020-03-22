04c772a1861440cf844cb309fe46d6b39e10b0ed
emi <ejhartline@gmail.com>
2020-03-22 12:16:09 -0400

    aesthetic changes (css, some html)

--- script.js
+++ script.js
@@ -2,20 +2,18 @@ $(document).ready(function () {
 
     // DEFINING VARIABLES AND FUNCTIONS
     // __________________________________________________________________________________________________________________________
-
     // DOM elements
     var weatherResults = $(".eight");
     var cityInputEl = $("#cityInput");
     var searchButton = $("button");
     var cityHistoryDiv = $(".city-history-div");
-    var cityNameDisplay = $("#cityDisplay");
-    var dateDisplay = $("#dateDisplay");
+    var cityDateDisplay = $("#cityDateDisplay");
     var weatherIcon = $("#weatherIcon");
     var tempDisplay = $("#temp");
     var humidDisplay = $("#humid");
     var windDisplay = $("#wind");
-    var uvDisplay = $("#uv");
-    var uvIndex = $("#uvIndex");
+    var uvDisplay = $("#uv-div");
+    var uvIndex = $("#uv");
     // 5-Day forecast DOM
     var day1 = $("#day1");
     var day2 = $("#day2");
@@ -46,17 +44,15 @@ $(document).ready(function () {
     var fourDays = moment().add(4, "days").format("l");
     var fiveDays = moment().add(5, "days").format("l");
 
-    // Display times
-    dateDisplay.text(" (" + today + ") ");
+    // Display dates (5-day forecast)
     day1.text(tomorrow);
     day2.text(twoDays);
     day3.text(threeDays);
     day4.text(fourDays);
     day5.text(fiveDays);
 
-    // function AJAX 0.5 (for using local storage instead of input for renderLastCity function)
+    // function AJAX 0.5 (uses local storage instead of search input for rendering weather when page revsited)
     var ajaxOneHalf = function () {
-
         // query variables AJAX call 0.5
         var apiKey = "2b3fd2d1da58a021b07a4c6c75acd193";
         var cityName = localStorage.getItem("city");
@@ -67,10 +63,8 @@ $(document).ready(function () {
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response);
-
             // API variables
-            var cityName = response.name;
+            var city = response.name;
             var tempK = response.main.temp;
             var tempF = (1.8 * (tempK - 271.15)) + 32;
             var humidity = response.main.humidity;
@@ -80,7 +74,7 @@ $(document).ready(function () {
 
             // display in HTML
             weatherResults.removeClass("hidden");
-            cityNameDisplay.text(cityName + " ");
+            cityDateDisplay.text(city + " (" + today + ") ");
             weatherIcon.attr("src", iconURL);
             tempDisplay.text("Temperature: " + tempF.toFixed(1) + " \xB0F");
             humidDisplay.text("Humidity: " + humidity + "%");
@@ -91,13 +85,13 @@ $(document).ready(function () {
             // cityHistoryDiv.append(cityHistory);
             // // TO DO: conditionals for limiting number of renders and repeats
 
+            // Then executes ajaxTwo and ajaxThree (nested)
             ajaxTwo();
         });
     };
 
     // function AJAX call 1, with AJAX call 2 inside
     var ajaxOne = function () {
-
         // Query URL variables
         var apiKey = "2b3fd2d1da58a021b07a4c6c75acd193";
         var cityName = cityInputEl.val().trim();
@@ -106,15 +100,13 @@ $(document).ready(function () {
         // store city: 
         localStorage.setItem("city", cityName);
 
-        // AJAX call 1
+        // AJAX call 1 - current weather data
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response);
-
             // API variables
-            var cityName = response.name;
+            var city = response.name;
             var tempK = response.main.temp;
             var tempF = (1.8 * (tempK - 271.15)) + 32;
             var humidity = response.main.humidity;
@@ -128,7 +120,7 @@ $(document).ready(function () {
 
             // display in HTML
             weatherResults.removeClass("hidden");
-            cityNameDisplay.text(cityName + " ");
+            cityDateDisplay.text(city + " (" + today + ") ");
             weatherIcon.attr("src", iconURL);
             tempDisplay.text("Temperature: " + tempF.toFixed(1) + " \xB0F");
             humidDisplay.text("Humidity: " + humidity + "%");
@@ -139,57 +131,52 @@ $(document).ready(function () {
             cityHistoryDiv.append(cityHistory);
             // TO DO: conditionals for limiting number of renders and repeats
 
-            // execute second AJAX call
+            // execute second AJAX call, with third nested
             ajaxTwo();
         });
     };
 
     // function AJAX call 2, with AJAX 3 nested inside
     var ajaxTwo = function () {
-
-        // query URL variables (2nd AJAX call)
+        // query URL variables (2nd AJAX call) - UV index
         var apiKey = "2b3fd2d1da58a021b07a4c6c75acd193";
         var lat = localStorage.getItem("lat");
         var lon = localStorage.getItem("lon");
         var uvQueryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
 
+        // ajax call 2 - UV index
         $.ajax({
             url: uvQueryURL,
             method: "GET"
         }).then(function (response2) {
-            console.log(response2);
-
             // API variables
             var uv = response2.value;
 
             // display in HTML
-            uvDisplay.text("UV Index: ");
-            uvIndex.text(uv);
+            uvIndex.text("UV Index: " + uv);
             // TO DO: conditionals for color coding uv index display background
         });
         ajaxThree();
     };
 
-    // function ajax call 3
+    // function ajax call 3 - 5Day forecast
     var ajaxThree = function () {
-
         // Query URL variables
         var apiKey = "2b3fd2d1da58a021b07a4c6c75acd193";
         var cityName = localStorage.getItem("city");
         var fiveQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
 
+        // third ajax call, 5-Day forcast
         $.ajax({
             url: fiveQueryURL,
             method: "GET"
         }).then(function (response3) {
-
-            // arrays, keeping DOM elements together by index number
+            // arrays, keeping DOM elements matched together by index number
             var icons = [icon1, icon2, icon3, icon4, icon5];
             var temps = [temp1, temp2, temp3, temp4, temp5];
             var humids = [humid1, humid2, humid3, humid4, humid5];
             // array, collecting only the response3 indexes needed for the 5-day forecast, index mathes its DOM element
             var day = [response3.list[0], response3.list[8], response3.list[16], response3.list[24], response3.list[32]];
-            console.log(day);
 
             for (var i = 0; i < 5; i++) {
                 // getting icon ID from each forecast in day array, and assigning its icon url to matching image in DOM
@@ -212,7 +199,7 @@ $(document).ready(function () {
     };
 
     // Function to render last city search at beginning of page refresh
-    // won't render anythign is local storage is empty
+    // won't render anything if local storage is empty
     var renderLastCity = function () {
         ajaxOneHalf();
     };
@@ -225,7 +212,6 @@ $(document).ready(function () {
 
     // EXECUTING FUNCTIONS
     // __________________________________________________________________________________________________________________________
-
     // Render last search on refresh
     renderLastCity();
 
